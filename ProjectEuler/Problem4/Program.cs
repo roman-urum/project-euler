0f810fa0c715540d7775ab04b202fada00824273
rurum <rurum@provectus.com>
2019-07-08 12:24:51 +0300

    add problem4 solution

---
+++ ProjectEuler/Problem4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Problem4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var maxPalindrome = 0;
+            for (int i = 999; i > 99; i--)
+            {
+                //decrement by 11 because result palindrome must be devisible by 11. Details here https://projecteuler.net/thread=4#1211
+                for (int j = 990; j > 99; j -= 11)
+                {
+                    int k = i * j;
+                    if (isPalindrome(k))
+                    {
+                        if (maxPalindrome < k)
+                        {
+                            maxPalindrome = k;
+                        }
+                    }
+                }
+            }
+            stopWatch.Stop();
+            Console.WriteLine($"{maxPalindrome}, elapse {stopWatch.Elapsed.TotalMilliseconds} ms.");
+        }
+
+        public static bool isPalindrome(int d)
+        {
+            var dString = d.ToString();
+            var l = dString.Length;
+            for (int i = 0; i < l / 2; i++)
+            {
+                if (dString[i] != dString[l - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
